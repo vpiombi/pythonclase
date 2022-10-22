c99ee943cfa94503f4d110e02c4bb016ee68c770
Sofi <sorieta@itba.edu.ar>
2022-10-22 18:52:38 -0300

    info.datos

--- Prueba.py
+++ Prueba.py
@@ -1,5 +1,8 @@
 import pandas
+#Instalar import matplotlib.pyplot
 url = "https://raw.githubusercontent.com/vpiombi/pythonclase/main/imdb-videogames.csv"
 data = pandas.read_csv(url)
 print(data)
 
+#Primero entendemos los formatos de los datos con los que contamos 
+data.info()
