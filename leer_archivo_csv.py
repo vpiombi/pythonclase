c9febc4b4e2e545b2be358be275983da93356126
Valentina <vpiombi@gmail.com>
2022-10-24 13:50:12 -0300

    leer_csv_01

--- leer_archivo_csv.py
+++
@@ -1,5 +0,0 @@
-import csv
-with open ('./pythonclase/imdb-videogames.csv') as File:
-    misDatosReunion= csv.DictReader (File, delimiter=';')
-    for asistente in misDatosReunion:
-        print(asistente['Nombre']+' - ' + asistente['Correo'] + ' - ' + asistente['Rol'])
