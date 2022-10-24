712bbf9bd495e8ba8eaaf613d3bcf16490bf2405
Sofi <sorieta@itba.edu.ar>
2022-10-24 15:58:10 -0300

    cammbio

--- Prueba.py
+++ Prueba.py
@@ -1,8 +1,25 @@
 import pandas
-#Instalar import matplotlib.pyplot
+import matplotlib
 url = "https://raw.githubusercontent.com/vpiombi/pythonclase/main/imdb-videogames.csv"
 data = pandas.read_csv(url)
 print(data)
 
-#Primero entendemos los formatos de los datos con los que contamos 
-data.info()
+
+data.drop(['Unnamed: 0', 'url'], axis=1, inplace=True)
+data.head()
+
+data.columns
+
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+
+filepath = "https://raw.githubusercontent.com/vpiombi/pythonclase/main/imdb-videogames.csv"
+df = pd.read_csv(filepath)
+
+df.columns
