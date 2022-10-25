b4a51a97125991875bcca3975cc855f276b9d768
Sofi <sorieta@itba.edu.ar>
2022-10-24 23:26:31 -0300

    grafico ejemplo

--- Prueba.py
+++ Prueba.py
@@ -1,10 +1,27 @@
-import pandas
-import matplotlib
-url = "https://raw.githubusercontent.com/vpiombi/pythonclase/main/imdb-videogames.csv"
-data = pandas.read_csv(url)
-print(data)
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
 
-#Primero entendemos los formatos de los datos con los que contamos 
-data.info()
 
-df.columns
+data = pd.read_csv('https://raw.githubusercontent.com/vpiombi/pythonclase/main/imdb-videogames.csv')
+
+
+
+data['votes']= data['votes'].str.replace(',','').astype(float)
+data['votes'] = data['votes'].round(2)
+
+data.loc[:,['votes']]
+
+data.drop(['Unnamed: 0', 'url'], axis=1, inplace=True)
+data.head()
+
+generoxanio = data[['year', 'Action', 'Adventure', 'Comedy', 'Crime', 'Family', 'Fantasy',
+                    'Mystery', 'Sci-Fi', 'Thriller']].groupby('year').sum()
+generoxanio
+
+generoxanio = generoxanio[generoxanio.index <= 2022]
+
+fig = px.line(generoxanio, x=generoxanio.index, y=generoxanio.columns, title='Number of games published',
+             color_discrete_sequence=px.colors.qualitative.Bold)
+fig.show()
