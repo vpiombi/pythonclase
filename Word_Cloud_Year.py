8ff37dcbe195e6dbfe011d6e021dfd172a0c1018
Valentina <vpiombi@gmail.com>
2022-10-27 19:55:55 -0300

    word_cloud_final

--- Word_Cloud_Year.py
+++ Word_Cloud_Year.py
@@ -18,6 +18,19 @@ data['plot']= data['plot'].str.replace('Add','').astype(np.object)
 data['plot']= data['plot'].str.replace('Plot','').astype(np.object)
 data['plot']= data['plot'].str.replace('Full','').astype(np.object)
 data['plot']= data['plot'].str.replace('full','').astype(np.object)
+data['plot']= data['plot'].str.replace('summary','').astype(np.object)
+data['plot']= data['plot'].str.replace('set','').astype(np.object)
+data['plot']= data['plot'].str.replace('See','').astype(np.object)
+data['plot']= data['plot'].str.replace('one','').astype(np.object)
+data['plot']= data['plot'].str.replace('back','').astype(np.object)
+data['plot']= data['plot'].str.replace('take','').astype(np.object)
+data['plot']= data['plot'].str.replace('make','').astype(np.object)
+data['plot']= data['plot'].str.replace('will','').astype(np.object)
+data['plot']= data['plot'].str.replace('now','').astype(np.object)
+data['plot']= data['plot'].str.replace('way','').astype(np.object)
+data['plot']= data['plot'].str.replace('find','').astype(np.object)
+data['plot']= data['plot'].str.replace('Two','').astype(np.object)
+data['plot']= data['plot'].str.replace('two','').astype(np.object)
 
 
 #Create a Word Cloud 
@@ -26,11 +39,11 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
 
 
-#Read plot from year 1962
+#Read plot from year 1960 a 1980
 stopwords=STOPWORDS
 
 
-filt= (data['year']== 1962) 
+filt= (data['year']<= 1980) & (data['year']>= 1960)
 
 
 wc= WordCloud(
@@ -44,13 +57,13 @@ wc= WordCloud(
 wc.generate(''.join(data.loc[filt,'plot']))
 
 #save the file
-wc.to_file('wordcloud_year_output1962.png')
+wc.to_file('wordcloud_year_output1960_1980.png')
 
-#Read plot from year 2022
+#Word Cloud de los plots del año 1980 a 2000
 stopwords=STOPWORDS
 
 
-filt= (data['year']== 2022) 
+filt= (data['year']<= 2000) & (data['year']>= 1980)
 
 
 wc= WordCloud(
@@ -64,5 +77,25 @@ wc= WordCloud(
 wc.generate(''.join(data.loc[filt,'plot']))
 
 #save the file
-wc.to_file('wordcloud_year_output2022.png')
+wc.to_file('wordcloud_year_output1980_2000.png')
+
+#Word Cloud de los plots del año 2000 a 2022
+stopwords=STOPWORDS
+
+
+filt= (data['year']<= 2022) & (data['year']>= 2000)
+
+
+wc= WordCloud(
+    background_color='white',
+    stopwords=stopwords,
+    height= 600,
+    width=400
+)
+
+
+wc.generate(''.join(data.loc[filt,'plot']))
+
+#save the file
+wc.to_file('wordcloud_year_output2000_2022.png')
 
