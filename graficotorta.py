5d18f3a8160759b0ad89f2f8b0e6449644b84968
Sofi <sorieta@itba.edu.ar>
2022-10-28 00:03:28 -0300

    graficotorta

--- graficotorta.py
+++ graficotorta.py
@@ -8,24 +8,15 @@ import altair as alt
 
 data = pd.read_csv('https://raw.githubusercontent.com/vpiombi/pythonclase/main/imdb-videogames.csv')
 
-data.info()
 
 data['votes']= data['votes'].str.replace(',','').astype(float)
 data['votes'] = data['votes'].round(2)
 
 data.loc[:,['votes']]
 
-data.drop(['Unnamed: 0', 'url'], axis=1, inplace=True)
+data.drop(['url'], axis=1, inplace=True)
 data.head()
 
 
-#Grafico con año y genero
-
-
-genero = data[['Action', 'Adventure', 'Comedy', 'Crime', 'Family', 'Fantasy', 'Mystery', 'Sci-Fi', 'Thriller']]
-genero
-
-
-grafico = px.pie(genero, values=genero, title='Cantidad de juegos publicados',
-             color_discrete_sequence=px.colors.qualitative.Plotly)
-grafico.show()
+fig = px.pie(data, values="Unnamed: 0", names='certificate')
+fig.show()
